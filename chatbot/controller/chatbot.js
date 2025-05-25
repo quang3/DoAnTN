bf3d253cf3eb2ee4e52a1959216819bb367b4bfd
quang3 <acuaaaaaa0505@gmail.com>
2025-05-25 22:13:08 +0700

    update chatbot

--- chatbot/controller/chatbot.js
+++ chatbot/controller/chatbot.js
@@ -1,16 +1,22 @@
 import OpenAI from "openai";
-
+import promptSytem from "../prompt/prompt-system.js";
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 export const chatbot = async (req, res) => {
     try {
-        const prompt = req.body.prompt || 'Xin chào';
-        const model = "ft:gpt-3.5-turbo-0125:personal:tmartbot:9QeT8Ohh";
+        const prompt = req.body.prompt || "Xin chào";
+        const model = "o4-mini";
         const completion = await openai.chat.completions.create({
-            messages: [{ role: "user", content: prompt }],
+            messages: [
+                {
+                    role: "system",
+                    content: promptSytem.data,
+                },
+                { role: "user", content: prompt },
+            ],
             model: model,
         });
         res.status(200).json(completion.choices[0].message.content);
@@ -19,4 +25,4 @@ export const chatbot = async (req, res) => {
         console.log(error);
         res.status(500).json({ error: error });
     }
-}
+};
